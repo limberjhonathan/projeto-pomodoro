16370b39cf6172019ec0848a039497ec02bfc3c0
Limber Jhonathan <limberjhonathan@gmail.com>
2024-10-15 23:04:52 -0300

    seconds ajustes

--- src/utils/seconds-to-time.ts
+++ src/utils/seconds-to-time.ts
@@ -1,6 +1,8 @@
+import { zeroLeft } from "./sero-left";
+
 export function secondsToTime(seconds: number): string {
-    const zeroLeft = (n: number) => Math.floor(n).toString().padStart(2, '0');
+    const hours = zeroLeft((seconds / 3600))
     const min = zeroLeft(Math.floor(seconds / 60));
     const sec = zeroLeft(seconds % 60);
-    return `${min}:${sec}`;
+    return `${hours}:${min}:${sec}`;
 }
